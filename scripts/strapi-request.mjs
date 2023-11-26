bba03bf36cd830c56f70bb20b32a13193ce51c7d
annatsherkavska <katotoka1985@gmail.com>
2023-11-26 16:08:47 +0100

    change link server

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,6 +1,7 @@
+import { writeFileSync } from 'node:fs';
 import qs from 'qs';
 
-const url = 'http://localhost:1337/api/reviews'
+const url = 'https://next-reviews-cms-4quo6d5fha-uc.a.run.app/api/reviews'
   + '?' + qs.stringify({
     fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
     populate: { image: { fields: ['url'] } },
@@ -10,4 +11,6 @@ const url = 'http://localhost:1337/api/reviews'
 const response = await fetch(url);
 const body = await response.json();
 const formatted = JSON.stringify(body, null, 2);
+const file = 'scripts/strapi-response.json';
+writeFileSync(file, formatted, 'utf8');
 //const imageUrl = body.data.map(item => item.attributes.image.data.attributes.url);
